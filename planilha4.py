b40c1dc9ea48bd0ec6f8edb140477130940ce4b2
Mateus-crio <132668503+Mateus-crio@users.noreply.github.com>
2023-10-06 13:47:52 -0300

    Add files via upload

---
+++ planilha4.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+
+link = 'https://mateus-crio.github.io/NovaForma/tabela5.html'
+response = requests.get(link)
+
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"}
+requisicao = requests.get(link, headers=headers)
+
+site = BeautifulSoup(requisicao.text,'html.parser')
+tables = site.find_all('table')
+print(tables)
+
+for table in enumerate(tables):
+    df = pd.read_html(str(table))
+    df[1].to_excel('Dados_ValoresdeFrete.xlsx')
